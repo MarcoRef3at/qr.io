c6fe340a04d02cd63ae67c8ca3e9bc75c71912ed
marcoref3at <marco_refaat@hotmail.com>
2023-04-18 02:14:12 +0200

    link logic

--- src/components/qrData/qrContents/qrLink.js
+++ src/components/qrData/qrContents/qrLink.js
@@ -1,12 +1,23 @@
 import * as React from 'react'
+import { useEffect, useState } from 'react'
 import TextInput from '../../shared/textInput'
 
 const QrLink = ({ setQrValue }) => {
+  const [url, setUrl] = useState('https://')
+
+  useEffect(() => {
+    setQrValue(url)
+  }, [url])
+
   const title = 'Submit URL'
   return (
     <div>
       <div className="contentTitle">{title}</div>
-      <TextInput label="URL" />
+      <TextInput
+        label="URL"
+        value={url}
+        handleChange={() => setUrl(event.target.value)}
+      />
     </div>
   )
 }
