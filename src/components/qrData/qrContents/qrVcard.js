ea702cd5385292464fdf73db05ee5523cef8553c
marcoref3at <marco_refaat@hotmail.com>
2023-04-21 08:21:44 +0200

    vcard styling

--- src/components/qrData/qrContents/qrVcard.js
+++ src/components/qrData/qrContents/qrVcard.js
@@ -24,25 +24,27 @@ const QrVcard = ({ setQrValue }) => {
         accept=".vcf"
         onChange={handleChange}
       />
-      <TextInput label="First Name" />
-      <TextInput label="Last Name" />
-      <TextInput label="Phone number" />
-      <TextInput label="Mobile" />
-      <TextInput label="E-mail" />
-      <TextInput label="Website (URL)" />
-      <TextInput label="Company" />
-      <TextInput label="Job title" />
-      <TextInput label="Fax" />
-      <TextField
-        id="filled-multiline-flexible"
-        label="Address"
-        multiline
-        maxRows={4}
-        variant="filled"
-      />
-      <TextInput label="City" />
-      <TextInput label="Post code" />
-      <TextInput label="Country" />
+      <div className="vcardForm">
+        <TextInput label="First Name" />
+        <TextInput label="Last Name" />
+        <TextInput label="Phone number" />
+        <TextInput label="Mobile" />
+        <TextInput label="E-mail" />
+        <TextInput label="Website (URL)" />
+        <TextInput label="Company" />
+        <TextInput label="Job title" />
+        <TextInput label="Fax" />
+        <TextField
+          id="filled-multiline-flexible"
+          label="Address"
+          multiline
+          maxRows={4}
+          variant="filled"
+        />
+        <TextInput label="City" />
+        <TextInput label="Post code" />
+        <TextInput label="Country" />
+      </div>
     </div>
   )
 }
--- src/components/shared/textField.js
+++ src/components/shared/textField.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Input } from 'reactstrap'
 
-export default function TextField({ handleChange, ...rest }) {
-  return <Input name="text" type="textarea" onChange={handleChange} {...rest} />
+export default function TextField({ handleChange, label, ...rest }) {
+  return (
+    <Input
+      name="text"
+      placeholder={label}
+      type="textarea"
+      onChange={handleChange}
+      {...rest}
+    />
+  )
 }
