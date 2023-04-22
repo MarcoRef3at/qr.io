d0ff6909975d886c15638e374af769a33d1c9f93
marcoref3at <marco_refaat@hotmail.com>
2023-04-22 03:31:48 +0200

    styling

--- src/components/qrData/qrPreview.js
+++ src/components/qrData/qrPreview.js
@@ -3,6 +3,7 @@ import '../../styles.scss'
 import { QRCode } from 'react-qrcode-logo'
 import html2canvas from 'html2canvas'
 import QrButton from '../shared/button'
+import { Col } from 'reactstrap'
 
 export default function QrPreview({ QrValue, QrSpecs }) {
   const handleDownload = () => {
@@ -17,74 +18,78 @@ export default function QrPreview({ QrValue, QrSpecs }) {
   }
   return (
     <div className="qrPreview">
-      <QRCode
-        value={QrValue}
-        logoOnLoad={() => console.log('logo loaded')}
-        {...{
-          ...QrSpecs,
-          eyeRadius: [
-            // build eyeRadius manually
-            {
-              outer: [
-                QrSpecs.eyeradius_0_outer_0,
-                QrSpecs.eyeradius_0_outer_1,
-                QrSpecs.eyeradius_0_outer_2,
-                QrSpecs.eyeradius_0_outer_3,
-              ],
-              inner: [
-                QrSpecs.eyeradius_0_inner_0,
-                QrSpecs.eyeradius_0_inner_1,
-                QrSpecs.eyeradius_0_inner_2,
-                QrSpecs.eyeradius_0_inner_3,
-              ],
-            },
-            {
-              outer: [
-                QrSpecs.eyeradius_1_outer_0,
-                QrSpecs.eyeradius_1_outer_1,
-                QrSpecs.eyeradius_1_outer_2,
-                QrSpecs.eyeradius_1_outer_3,
-              ],
-              inner: [
-                QrSpecs.eyeradius_1_inner_0,
-                QrSpecs.eyeradius_1_inner_1,
-                QrSpecs.eyeradius_1_inner_2,
-                QrSpecs.eyeradius_1_inner_3,
-              ],
-            },
-            {
-              outer: [
-                QrSpecs.eyeradius_2_outer_0,
-                QrSpecs.eyeradius_2_outer_1,
-                QrSpecs.eyeradius_2_outer_2,
-                QrSpecs.eyeradius_2_outer_3,
-              ],
-              inner: [
-                QrSpecs.eyeradius_2_inner_0,
-                QrSpecs.eyeradius_2_inner_1,
-                QrSpecs.eyeradius_2_inner_2,
-                QrSpecs.eyeradius_2_inner_3,
-              ],
-            },
-          ],
-          eyeColor: [
-            // build eyeColor manually
-            {
-              outer: QrSpecs.eyecolor_0_outer ?? QrSpecs.fgColor ?? '#000000',
-              inner: QrSpecs.eyecolor_0_inner ?? QrSpecs.fgColor ?? '#000000',
-            },
-            {
-              outer: QrSpecs.eyecolor_1_outer ?? QrSpecs.fgColor ?? '#000000',
-              inner: QrSpecs.eyecolor_1_inner ?? QrSpecs.fgColor ?? '#000000',
-            },
-            {
-              outer: QrSpecs.eyecolor_2_outer ?? QrSpecs.fgColor ?? '#000000',
-              inner: QrSpecs.eyecolor_2_inner ?? QrSpecs.fgColor ?? '#000000',
-            },
-          ],
-        }}
-      />
-      <QrButton onClick={handleDownload}>Download</QrButton>
+      <Col>
+        <QRCode
+          value={QrValue}
+          logoOnLoad={() => console.log('logo loaded')}
+          {...{
+            ...QrSpecs,
+            eyeRadius: [
+              // build eyeRadius manually
+              {
+                outer: [
+                  QrSpecs.eyeradius_0_outer_0,
+                  QrSpecs.eyeradius_0_outer_1,
+                  QrSpecs.eyeradius_0_outer_2,
+                  QrSpecs.eyeradius_0_outer_3,
+                ],
+                inner: [
+                  QrSpecs.eyeradius_0_inner_0,
+                  QrSpecs.eyeradius_0_inner_1,
+                  QrSpecs.eyeradius_0_inner_2,
+                  QrSpecs.eyeradius_0_inner_3,
+                ],
+              },
+              {
+                outer: [
+                  QrSpecs.eyeradius_1_outer_0,
+                  QrSpecs.eyeradius_1_outer_1,
+                  QrSpecs.eyeradius_1_outer_2,
+                  QrSpecs.eyeradius_1_outer_3,
+                ],
+                inner: [
+                  QrSpecs.eyeradius_1_inner_0,
+                  QrSpecs.eyeradius_1_inner_1,
+                  QrSpecs.eyeradius_1_inner_2,
+                  QrSpecs.eyeradius_1_inner_3,
+                ],
+              },
+              {
+                outer: [
+                  QrSpecs.eyeradius_2_outer_0,
+                  QrSpecs.eyeradius_2_outer_1,
+                  QrSpecs.eyeradius_2_outer_2,
+                  QrSpecs.eyeradius_2_outer_3,
+                ],
+                inner: [
+                  QrSpecs.eyeradius_2_inner_0,
+                  QrSpecs.eyeradius_2_inner_1,
+                  QrSpecs.eyeradius_2_inner_2,
+                  QrSpecs.eyeradius_2_inner_3,
+                ],
+              },
+            ],
+            eyeColor: [
+              // build eyeColor manually
+              {
+                outer: QrSpecs.eyecolor_0_outer ?? QrSpecs.fgColor ?? '#000000',
+                inner: QrSpecs.eyecolor_0_inner ?? QrSpecs.fgColor ?? '#000000',
+              },
+              {
+                outer: QrSpecs.eyecolor_1_outer ?? QrSpecs.fgColor ?? '#000000',
+                inner: QrSpecs.eyecolor_1_inner ?? QrSpecs.fgColor ?? '#000000',
+              },
+              {
+                outer: QrSpecs.eyecolor_2_outer ?? QrSpecs.fgColor ?? '#000000',
+                inner: QrSpecs.eyecolor_2_inner ?? QrSpecs.fgColor ?? '#000000',
+              },
+            ],
+          }}
+        />
+        <QrButton className=".row" onClick={handleDownload}>
+          Download
+        </QrButton>
+      </Col>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import * as React from 'react'
-import '../styles.scss'
+import { useEffect, useState } from 'react'
 import QrData from '../components/qrData'
 import QrPreview from '../components/qrData/qrPreview'
-import { useState } from 'react'
+import '../styles.scss'
 
 const IndexPage = () => {
   const [QrValue, setQrValue] = useState('')
   const [QrSpecs, setQrSpecs] = useState({
     bgColor: '#ffffff',
-    fgColor: '#000000',
+    fgColor: '#e3e3e3',
     size: 300,
   })
+  useEffect(() => {
+    console.log(QrValue)
+    if (QrValue && QrSpecs.fgColor == '#e3e3e3') {
+      setQrSpecs({ ...QrSpecs, fgColor: '#000000' })
+    }
+  }, [QrValue])
 
   return (
     <main className="pageStyles">
