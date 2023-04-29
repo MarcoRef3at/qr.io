fda32529885e9b991b64b2eb6187c1ab21f17494
marcoref3at <marco_refaat@hotmail.com>
2023-04-29 13:25:59 +0300

    advanced logo settings

--- src/components/qrData/qrCustomization/logos.js
+++ src/components/qrData/qrCustomization/logos.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { RiImageAddFill } from 'react-icons/ri'
 import { AccordionBody, AccordionHeader, AccordionItem } from 'reactstrap'
+import { QrContext } from '../../../contexts/qr.context'
 import { CheckboxField } from '../../shared/checkbox'
 import Slider from '../../shared/slider'
 import QrDropDown from './../../shared/dropDown'
@@ -9,6 +10,7 @@ import StaticLogos from './staticLogos'
 const logoPaddingStyles = ['square', 'circle']
 export default function Logos({ handleChange }) {
   const [logoPadding, setLogoPadding] = useState(logoPaddingStyles[0])
+  const { QrSpecs } = useContext(QrContext)
 
   useEffect(() => {
     const target = {
@@ -18,6 +20,10 @@ export default function Logos({ handleChange }) {
     handleChange({ target })
   }, [logoPadding])
 
+  useEffect(() => {
+    console.log('QrSpecs', QrSpecs.logoImage)
+  }, [QrSpecs])
+
   return (
     <AccordionItem>
       <AccordionHeader targetId="2">
@@ -26,50 +32,54 @@ export default function Logos({ handleChange }) {
       </AccordionHeader>
       <AccordionBody accordionId="2" className="logo">
         <StaticLogos handleChange={handleChange} />
-        <Slider
-          name="logoWidth"
-          label="logoWidth"
-          handleChange={handleChange}
-          min={20}
-          max={500}
-        />
-        <Slider
-          name="logoHeight"
-          label="logoHeight"
-          handleChange={handleChange}
-          min={20}
-          max={500}
-        />
-        <Slider
-          name="logoOpacity"
-          label="logoOpacity"
-          handleChange={handleChange}
-          min={0}
-          max={1}
-          step={0.1}
-          defaultValue={1}
-        />
+        {QrSpecs.logoImage && (
+          <div>
+            <Slider
+              name="logoWidth"
+              label="logoWidth"
+              handleChange={handleChange}
+              min={20}
+              max={500}
+            />
+            <Slider
+              name="logoHeight"
+              label="logoHeight"
+              handleChange={handleChange}
+              min={20}
+              max={500}
+            />
+            <Slider
+              name="logoOpacity"
+              label="logoOpacity"
+              handleChange={handleChange}
+              min={0}
+              max={1}
+              step={0.1}
+              defaultValue={1}
+            />
 
-        <CheckboxField
-          name="removeQrCodeBehindLogo"
-          handleChange={handleChange}
-        />
-        <Slider
-          name="logoPadding"
-          label="Logo Padding"
-          handleChange={handleChange}
-          min={0}
-          max={20}
-          step={1}
-          defaultValue={0}
-        />
+            <CheckboxField
+              name="removeQrCodeBehindLogo"
+              handleChange={handleChange}
+            />
+            <Slider
+              name="logoPadding"
+              label="Logo Padding"
+              handleChange={handleChange}
+              min={0}
+              max={20}
+              step={1}
+              defaultValue={0}
+            />
 
-        <QrDropDown
-          label="Logo Padding Style"
-          options={logoPaddingStyles}
-          option={logoPadding}
-          setOption={setLogoPadding}
-        />
+            <QrDropDown
+              label="Logo Padding Style"
+              options={logoPaddingStyles}
+              option={logoPadding}
+              setOption={setLogoPadding}
+            />
+          </div>
+        )}
       </AccordionBody>
     </AccordionItem>
   )
